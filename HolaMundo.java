a1ea58a2afef56bf0931d2b20f76ef8d8fdeff44
Mario-Chura <mchurapum@unsa.edu.pe>
2022-04-29 20:52:16 -0500

    Añadiendo linea de bienvenida

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		//Hola Mundo
         System.out.println("Hola Mundo");
-
+        System.out.println("Bienvenidos a las clases de laboratorio Pweb2 Grupo E");
 	}
 
 }
